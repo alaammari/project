3de8d39a1e963b00348580391c7b0787793e4d11
ALA AMMERI <alaameri10@gmail.com>
2024-10-31 18:06:41 +0100

    Initial commit

---
+++ api/frontend/Controllers/ProductController1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace frontend.Controllers
+{
+    public class ProductController1 : Controller
+    {
+        Uri baseAddress= new Uri("https://localhost:44333/api")
+            private readonly HttpClient _Client;
+        public ProductController1()
+        {
+            _Client = new HttpClient();
+            _Client.BaseAddress = baseAddress;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Product> products = new List<Product>();
+            HttpResponseMessage response = _Client.GetAsync(_Client.BaseAddress + "/product/Get");
+            return View();
+        }   
+    }
+}
---
+++ api/frontend/Models/Products.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace frontend.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        [DisplayName("product name")]
+        public required string Name { get; set; }
+        public required string Sku { get; set; }
+    }
+}
